e8c3cd3e1dc5f2a44c844596999a515674a5c903
孟远 <mengyuan@yapingguo.com>
2016-05-18 15:35:08 +0800

    update

---
+++ app/src/main/java/com/my/controller/MainActivity.java
@@ -0,0 +1,58 @@
+package com.my.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import com.my.R;
+import com.my.view.CarouselFigureView;
+import com.my.view.switchanimotion.DepthPageTransformer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CarouselFigureView carouselFigureView;
+
+    /*
+    http://o.ypgimg.com/content/2016/5/16/3b61698c-cf8b-4abe-9c39-d474b454d790.jpg
+    http://o.ypgimg.com/content/2016/5/16/88d50d38-09fb-4d3e-bd0c-fc52e2a26c74.jpg
+    http://o.ypgimg.com/content/2016/5/16/e5a7b0e3-1a21-4e66-adc8-17a1b7c1a92f.jpg
+    http://o.ypgimg.com/content/2016/5/16/8bae5357-d542-4e52-ad5f-ad7d021fc163.jpg
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        carouselFigureView = (CarouselFigureView) findViewById(R.id.carousel_figure_view);
+        ArrayList<String> url = new ArrayList<>();
+
+        url.add("http://o.ypgimg.com/content/2016/5/16/3b61698c-cf8b-4abe-9c39-d474b454d790.jpg");
+        url.add("http://o.ypgimg.com/content/2016/5/16/88d50d38-09fb-4d3e-bd0c-fc52e2a26c74.jpg");
+        url.add("http://o.ypgimg.com/content/2016/5/16/e5a7b0e3-1a21-4e66-adc8-17a1b7c1a92f.jpg");
+        url.add("http://o.ypgimg.com/content/2016/5/16/8bae5357-d542-4e52-ad5f-ad7d021fc163.jpg");
+
+        ArrayList<Integer> resourceList =new ArrayList<>();
+        resourceList.add(R.drawable.aaa);
+        resourceList.add(R.mipmap.ic_launcher);
+        resourceList.add(R.mipmap.img_empty);
+
+        carouselFigureView.setURL(url);
+//        carouselFigureView.setResourceList(resourceList);
+        carouselFigureView.startLoad();
+        carouselFigureView.setViewPagerSwitchStyle(new DepthPageTransformer());
+        carouselFigureView.setViewPagerSwitchSpeed(200);
+        carouselFigureView.setCarouselFigureItemClickListener(new CarouselFigureView.CarouselFigureItemClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+
+                Log.i("mengyuan","position:::"+position);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/my/view/CarouselFigureView.java
@@ -0,0 +1,414 @@
+package com.my.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.DrawableRes;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.bumptech.glide.Glide;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.my.R;
+import com.my.view.switchanimotion.ViewPagerScroller;
+
+/**
+ * AUTHOR:       Yuan.Meng
+ * E-MAIL:       mengyuanzz@126.com
+ * CREATE-TIME:  16/5/16/上午10:49
+ * DESC:
+ */
+public class CarouselFigureView extends RelativeLayout {
+
+    //----------－－－－-控件－－－－－－－－－－－
+    private ViewPager viewPager;//ImageView容器
+    private LinearLayout indicationPointLayout; //指示点容器
+
+    //-------------－－自定义属性－－－－－－－－－－－－
+    private boolean isAutoPlay = true; //是否自动切换
+    private boolean isInfiniteLoop = true; //是否无限循环
+    private boolean isNeedIndicationPoint = true;//是否需要指示点
+    private float pointLeft_Right_Margin = 13;//指示点的间距，单位是px,在布局文件中设置单位最好为dp
+    private float pointBottomMargin = 5;//指示点上移的距离,单位是px，在布局文件中设置单位最好为dp
+    private int playIntervalTime = 3000;//自动切换间隔，默认3秒，单位毫秒
+    private int pointBackgroundId = R.drawable.point_bg;//指示点背景，可自定义
+
+    //－－－－－－－－－数据－－－－－－－－－－－
+    private int itemCount; //条目数量
+    private int minCount;// 因为ViewPager无限循环切换时,条目数量小于等于3会出现崩溃、显示空白等问题，所以当数量为3以下时，该变量用于将条目数量翻倍
+    private int lastPosition;//上一个显示的条目下标
+    private List<String> urlList;//url集合
+    private List<Integer> resourceList;//资源集合
+    private List<ImageView> pictureList;//存放图片的ImageView
+    //--------------其他－－－－－－－－－－－
+    private boolean userIsTouchScreen = false; //用户是否触屏，当用户触屏时，应停止自动播放
+    private int MAX_VALUE = Integer.MAX_VALUE; //最大值，用于无限循环
+    private int SELCET_LOAD_MODE; //图片加载模式
+    private CarouselFigureItemClickListener listener;//条目点击事件
+
+    /**
+     * 图片的加载方式
+     */
+    public interface LOAD_MODE {
+        int URL = 1001, RESOURCE = 1002;
+    }
+
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (isAutoPlay) {
+                if (!userIsTouchScreen) {
+                    viewPager.setCurrentItem(lastPosition + 1);
+                }
+                handler.sendEmptyMessageDelayed(88, playIntervalTime);
+            }
+        }
+    };
+
+
+    public CarouselFigureView(Context context) {
+        this(context, null);
+    }
+
+    public CarouselFigureView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CarouselFigureView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        parseCustomAttributes(context, attrs);
+
+        initView(context, attrs, defStyleAttr);
+    }
+
+    private void initView(Context context, AttributeSet attrs, int defStyleAttr) {
+
+
+        initViewPager(context);
+
+        if (isNeedIndicationPoint) {
+            initPonitLinearLayout(context);
+        }
+
+    }
+
+    /**
+     * 解析自定义属性
+     */
+    private void parseCustomAttributes(Context context, AttributeSet attrs) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CarouselFigureView);
+        // 获得xml里定义的属性,格式为 名称_属性名 后面是默认值
+        isAutoPlay = a.getBoolean(R.styleable.CarouselFigureView_isAutoPlay, true);
+        isInfiniteLoop = a.getBoolean(R.styleable.CarouselFigureView_isInfiniteLoop, true);
+        isNeedIndicationPoint = a.getBoolean(R.styleable.CarouselFigureView_isNeedIndicationPoint, true);
+        pointLeft_Right_Margin = a.getDimension(R.styleable.CarouselFigureView_pointLeft_Right_Margin, 5);
+        pointBottomMargin = a.getDimension(R.styleable.CarouselFigureView_pointBottomMargin, 2);
+        playIntervalTime = a.getInt(R.styleable.CarouselFigureView_playIntervalTime, 3000);
+        pointBackgroundId = a.getResourceId(R.styleable.CarouselFigureView_pointBackground, R.drawable.point_bg);
+        // 为了保持以后使用该属性一致性,返回一个绑定资源结束的信号给资源
+        a.recycle();
+    }
+
+    /**
+     * 初始化指示点布局
+     *
+     * @param context
+     */
+    private void initPonitLinearLayout(Context context) {
+        indicationPointLayout = new LinearLayout(context);
+        indicationPointLayout.setOrientation(LinearLayout.HORIZONTAL);
+        indicationPointLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+
+
+        RelativeLayout.LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(ALIGN_PARENT_BOTTOM);
+        this.addView(indicationPointLayout, layoutParams);
+
+
+    }
+
+
+    /**
+     * 初始化ViewPager
+     *
+     * @param context
+     */
+    private void initViewPager(Context context) {
+        viewPager = new ViewPager(context);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (isNeedIndicationPoint) {
+                    indicationPointLayout.getChildAt(lastPosition % itemCount).setSelected(false);
+                    indicationPointLayout.getChildAt(position % itemCount).setSelected(true);
+                }
+                lastPosition = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                switch (state) {
+                    case ViewPager.SCROLL_STATE_IDLE://空闲状态
+                        userIsTouchScreen = false;
+                        break;
+                    case ViewPager.SCROLL_STATE_DRAGGING://用户滑动状态
+                        userIsTouchScreen = true;
+                        break;
+                }
+
+            }
+        });
+        RelativeLayout.LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        this.addView(viewPager, layoutParams);
+    }
+
+
+    /**
+     * set data
+     *
+     * @param urlList：图片的url地址
+     */
+    public void setURL(List<String> urlList) {
+        if (urlList == null || urlList.size() <= 0)
+            throw new RuntimeException(getContext().getString(R.string.setData_Exception));
+        this.urlList = urlList;
+        SELCET_LOAD_MODE = LOAD_MODE.URL;
+        minCount = itemCount = urlList.size();
+
+        parseData(urlList);
+    }
+
+
+    /**
+     * set data
+     *
+     * @param resourceList 图片资源地址
+     */
+    public void setResourceList(@DrawableRes List<Integer> resourceList) {
+        if (resourceList == null || resourceList.size() <= 0)
+            throw new RuntimeException(getContext().getString(R.string.setData_Exception));
+        this.resourceList = resourceList;
+        SELCET_LOAD_MODE = LOAD_MODE.RESOURCE;
+        minCount = itemCount = resourceList.size();
+        parseData(resourceList);
+    }
+
+    private void parseData(List listData) {
+        if (listData.size() == 1) {
+            isAutoPlay = false;
+            isInfiniteLoop = false;
+
+        } else if (isInfiniteLoop) {
+            if (listData.size() <= 3) {
+                minCount = itemCount * 2;
+                switch (SELCET_LOAD_MODE) {
+                    case LOAD_MODE.RESOURCE:
+                        this.resourceList.addAll(listData);
+                        break;
+                    case LOAD_MODE.URL:
+                        this.urlList.addAll(listData);
+                        break;
+                }
+            }
+        }
+
+
+    }
+
+    public void setCarouselFigureItemClickListener(CarouselFigureItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    /**
+     * 改变ViewPager的切换方式
+     *
+     * @param transformer 可自定义ViewPager.PageTransformer，此类代表ViewPager切换动画
+     */
+    public void setViewPagerSwitchStyle(ViewPager.PageTransformer transformer) {
+        viewPager.setPageTransformer(true, transformer);
+
+    }
+
+    /**
+     * 改变ViewPager的切换速度
+     *
+     * @param switchSpeed 单位毫秒
+     */
+    public void setViewPagerSwitchSpeed(int switchSpeed) {
+        if (switchSpeed >= playIntervalTime)
+            throw new RuntimeException(getContext().getString(R.string.setViewPagerSwitchSpeedException));
+
+        try {
+            Field field = ViewPager.class.getDeclaredField("mScroller");
+            field.setAccessible(true);
+            ViewPagerScroller scroller = new ViewPagerScroller(this.getContext(),
+                    new AccelerateInterpolator());
+            field.set(viewPager, scroller);
+            scroller.setmDuration(switchSpeed);
+        } catch (Exception e) {
+        }
+    }
+
+
+    /**
+     * 在设置完数据之后调用此方法进行显示
+     */
+    public void startLoad() {
+
+        if (SELCET_LOAD_MODE == 0)
+            throw new RuntimeException(getContext().getString(R.string.startException));
+        if (isNeedIndicationPoint) {
+            addIndicationPoint();
+        }
+        loadPictureToImageView();
+        viewPager.setAdapter(new MyViewPagerAdapter());
+        if (isInfiniteLoop) {
+            viewPager.setCurrentItem(MAX_VALUE / 2 - (MAX_VALUE / 2) % itemCount);
+        }
+        if (isAutoPlay) {
+            handler.sendEmptyMessageDelayed(88, playIntervalTime);
+        }
+    }
+
+    /**
+     * 根据数据将图片加载至ImageView，并将ImageView保存至List集合中
+     */
+    private void loadPictureToImageView() {
+        pictureList = new ArrayList<>();
+        ImageView imageView;
+        for (int i = 0; i < minCount; i++) {
+            imageView = new ImageView(this.getContext());
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            switch (SELCET_LOAD_MODE) {
+                case LOAD_MODE.URL:
+                    loadImgByUrl(urlList.get(i), imageView);
+                    break;
+                case LOAD_MODE.RESOURCE:
+                    loadImgByResourceId(resourceList.get(i), imageView);
+                    break;
+            }
+            pictureList.add(imageView);
+        }
+
+    }
+
+
+    /**
+     * 添加指示点
+     */
+    private void addIndicationPoint() {
+        ImageView pointImageView;
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins((int) pointLeft_Right_Margin, 0, (int) pointLeft_Right_Margin, (int) pointBottomMargin);
+
+        for (int i = 0; i < itemCount; i++) {
+            pointImageView = new ImageView(this.getContext());
+            pointImageView.setBackgroundResource(pointBackgroundId);
+            if (i == 0) {
+                pointImageView.setSelected(true);
+            } else {
+                pointImageView.setSelected(false);
+            }
+
+            indicationPointLayout.addView(pointImageView, layoutParams);
+        }
+    }
+
+
+    private class MyViewPagerAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            if (isInfiniteLoop) {
+                return MAX_VALUE;
+            } else {
+                return itemCount;
+            }
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, final int position) {
+            ImageView imageView;
+            if (itemCount < 4) {
+                imageView = pictureList.get(position % minCount);
+                Log.i("mengyuan", "itemCount<4:::" + position % minCount);
+            } else {
+                Log.i("mengyuan", "itemCount>=4:::" + position % itemCount);
+                imageView = pictureList.get(position % itemCount);
+            }
+
+            imageView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        listener.onClick(v, position % itemCount);
+                    }
+                }
+            });
+            container.addView(imageView);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            if (itemCount < 4) {
+                container.removeView(pictureList.get(position % minCount));
+            } else {
+                container.removeView(pictureList.get(position % itemCount));
+            }
+
+        }
+    }
+
+
+    public interface CarouselFigureItemClickListener {
+        void onClick(View view, int position);
+    }
+
+    //------------------other----------------------------------
+
+
+    private void loadImgByUrl(String url, final ImageView imageView) {
+        Glide.with(this.getContext())
+                .load(url)
+                .placeholder(R.mipmap.img_empty)
+                .crossFade()
+
+                .into(imageView);
+    }
+
+    private void loadImgByResourceId(@DrawableRes int resource, ImageView imageView) {
+        Glide.with(this.getContext())
+                .load(resource)
+                .placeholder(R.mipmap.img_empty)
+                .crossFade()
+                .into(imageView);
+    }
+
+
+}
